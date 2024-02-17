4ba64daacfaa098d8d13056e7f8011b769a06b8c
a22310439 <a22310439@ceti.mx>
2024-02-16 22:58:22 -0600

    Se corrigen errores y pulen detalles del programa de listas enlazadas. También se agregan sobrecargas de operadores (int, ++ y -).

--- Estructuras de datos/Lista enlazada/LinkedList.cpp
+++ Estructuras de datos/Lista enlazada/LinkedList.cpp
@@ -2,17 +2,19 @@
 
 using namespace std;
 
+const int EXIT_CODE = 0;
+
 template <typename t>
 class Node {
 public:
-    int value;
+    t value;
     Node* next;
     Node() {
-        this->next = NULL;
+        this->next = nullptr;
     }
     Node(t value) {
         this->value = value;
-        this->next = NULL;
+        this->next = nullptr;
     }
 };
 
@@ -20,41 +22,52 @@ template <typename t>
 class List {
 private:
     Node<t>* head;
+    int size;
 public:
     List<t>() {
         head = NULL;
+        size = 0;
     }
+    //Verificar el tamaño de la lista
+    int Size(){
+        return size;
+    }
+    // Agregar un valor al final
     void append(t value) {
-        Node<t>* newNode = new Node<t>();
-        newNode->value = value;
-        newNode->next = nullptr;
+        Node<t>* newNode = new Node<t>(value);
         if (head == nullptr) {
             head = newNode;
-        }
-        else {
+        }else {
             Node<t>* temp = head;
             while (temp->next != nullptr) {
                 temp = temp->next;
             }
             temp->next = newNode;
         }
+        size++;
     }
-
+    // Agregar un valor al inicio
     void prepend(t value) {
-        Node<t>* newNode = new Node<t>();
-        newNode->value = value;
+        Node<t>* newNode = new Node<t>(value);
         newNode->next = head;
         head = newNode;
+        size++;
     }
-
+    // Insertar un valor
     void insert(t value, int pos) {
-        Node<t>* newNode = new Node<t>();
-        newNode->value = value;
-        if (pos == 0) {
-            newNode->next = head;
-            head = newNode;
+        //Verificar que la posición no sea negativa
+        if (pos < 0) {
+            cout << "La posicion no puede ser negativa." << endl;
+            return;
         }
-        else {
+        Node<t>* newNode = new Node<t>(value); 
+        if (pos == 0) {             // Si la posicion es 0, usar prepend
+            prepend(value);
+        }else if(pos >= Size()) {   //Si la posicion es mayor o igual al tamaño de la lista, usar append
+            cout << "La posicion es mayor que el tamaño de la lista, " <<
+            "se agregara al final de la lista." << endl;
+            append(value);
+        }else {                     //Si la posicion es válida, insertar el nodo en la posición dada
             Node<t>* temp = head;
             for (int i = 0; i < pos - 1; i++) {
                 temp = temp->next;
@@ -62,25 +75,38 @@ public:
             newNode->next = temp->next;
             temp->next = newNode;
         }
+        size++;
     }
-
-    void remove(int pos) {
-        if (pos == 0) {
+    // Eliminar un valor
+    void remove(int posicion) {
+        // Verificar si la lista está vacía
+        if (listaVacia()) {
+            return;
+        }
+        // Verificar si la posición es válida
+        while (posicion < 1 || posicion > Size()) {
+            cout << "\nLa posicion no existe, intentalo de nuevo: " ;
+            cin >> posicion;
+            validarValor(posicion);
+        }
+        posicion--;
+        // Eliminar el nodo en la posición dada
+        if (posicion == 0) {
             Node<t>* temp = head;
             head = head->next;
             delete temp;
-        }
-        else {
+        } else {
             Node<t>* temp = head;
-            for (int i = 0; i < pos - 2; i++) {
+            for (int i = 0; i < posicion - 1; i++) {
                 temp = temp->next;
             }
             Node<t>* temp2 = temp->next;
             temp->next = temp2->next;
             delete temp2;
         }
+        size--;
     }
-
+    // Imprimir la lista
     void print() {
         Node<t>* temp = head;
         cout << endl;
@@ -90,7 +116,56 @@ public:
         }
         cout << endl;
     }
-
+    //Overload del operador int
+    operator int() {
+        int suma = 0;
+        Node<t>* temp = head;
+        while(temp != nullptr) {
+            suma += temp->value;
+            temp = temp->next;
+        }
+        return suma;
+    }
+    //Overload del operador ++
+    List operator ++() {
+        Node<t>* temp = head;
+        while(temp != nullptr) {
+            temp->value++;
+            temp = temp->next;
+        }
+        return *this;
+    }
+    //Overload del operador - (int)
+    List operator -(int valor) {     //Restar un valor a todos los elementos de la lista<int>
+        Node<t>* temp = head;
+        while(temp != nullptr) {
+            temp->value -= valor;
+            temp = temp->next;
+        }
+        return *this;
+    }
+    //Overload del operador - (float)
+    List operator -(float valor) {   //Restar un valor a todos los elementos de la lista<float>
+        if(valor < 0) {
+            cout << "El valor no puede ser negativo." << endl;
+            return *this;
+        }
+        Node<t>* temp = head;
+        while(temp != nullptr) {
+            temp->value -= valor;
+            temp = temp->next;
+        }
+        return *this;
+    }
+    //Verificar si la lista está vacía
+    bool listaVacia() {
+        if(head == nullptr) {
+            cout << "\nLa lista esta vacia." << endl;
+            return true;
+        }
+        return false;
+    }
+    //Destructor
     ~List() {
         Node<t>* temp = head;
         while (temp != nullptr) {
@@ -101,69 +176,212 @@ public:
     }
 };
 
+// Validar que el valor ingresado sea del tipo correcto
 template <typename t>
-void funcionLista(int opcion, List<t>& list) {
-    int valor, pos;
-    while (opcion != 0) {
-        cout << "\n1) Agregar un valor al inicio de la lista\n2) Agregar un valor al final de la lista\n3) Eliminar un valor\n4) Imprimir la lista\n0) Salir\n\nOpcion: ";
+void validarValor(t& valor){
+    while(cin.fail()) {
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        cout << "\nValor no valido, intentalo de nuevo: ";
+        cin >> valor;
+    }
+}
+
+// Validar que la opción ingresada sea del tipo correcto
+void validarOpcion(int& opcion, int max) {
+    while(cin.fail() || opcion < 0 || opcion > max) {
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        cout << "\nOpcion no valida, intentalo de nuevo: ";
         cin >> opcion;
+    }
+}
+
+// Función para caracteres y cadenas de texto
+template <typename t>
+void menuCaracteres(List<t>& list) {
+    int opcion, posicion;
+    t valor;
+    do {
+        cout << "\n1) Agregar un valor al inicio de la lista\n" <<
+                "2) Agregar un valor al final de la lista\n" <<
+                "3) Insertar un valor en una posicion dada\n" <<
+                "4) Eliminar un valor\n" <<
+                "5) Imprimir la lista\n" <<
+                "0) Salir\n\nOpcion: ";
+        cin >> opcion;
+        validarOpcion(opcion, 5);
         switch (opcion) {
             case 1: {
                 cout << "\nIngresa el valor a agregar: ";
                 cin >> valor;
-                list.append(valor);
+                validarValor(valor);
+                list.prepend(valor);
                 break;
             }
             case 2: {
                 cout << "\nIngresa el valor a agregar: ";
                 cin >> valor;
-                list.prepend(valor);
+                validarValor(valor);
+                list.append(valor);
                 break;
             }
             case 3: {
-                cout << "\nIngresa la posicion a eliminar: ";
-                cin >> pos;
-                list.remove(pos);
+                cout << "\nIngresa el valor a agregar: ";
+                cin >> valor;
+                validarValor(valor);
+                cout << "\nIngresa la posicion a insertar: ";
+                cin >> posicion;
+                list.insert(valor, posicion);
                 break;
             }
             case 4: {
+                cout << "\nIngresa la posicion a eliminar: ";
+                cin >> posicion;
+                validarValor(posicion);
+                list.remove(posicion);
+                break;
+            }
+            case 5: {
                 list.print();
                 break;
             }
-            case 0: {
+            case EXIT_CODE: {
+                cout << "\nRegresando al menu principal\n\n";
                 break;
             }
         }
-    }
+    }while (opcion != EXIT_CODE);
 }
+
+template <typename t>
+void menuNumeros(List<t>& list) {
+    int opcion, posicion;
+    t valor;
+    do {
+        cout << "\n1) Agregar un valor al inicio de la lista\n" <<
+                "2) Agregar un valor al final de la lista\n" <<
+                "3) Insertar un valor en uan posicion dada\n" <<
+                "4) Eliminar un valor\n" <<
+                "5) Imprimir todos los valores de la lista sumados\n" <<
+                "6) Sumar 1 a todos los valores de la lista\n" <<
+                "7) Restar un valor a todos los elementos de la lista\n" <<
+                "8) Imprimir la lista\n" <<
+                "0) Salir\n\n" <<
+                "Opcion: ";
+        cin >> opcion;
+        validarOpcion(opcion, 6);
+        switch (opcion) {
+            case 1: {
+                cout << "\nIngresa el valor a agregar: ";
+                cin >> valor;
+                validarValor(valor);
+                list.prepend(valor);
+                break;
+            }
+            case 2: {
+                cout << "\nIngresa el valor a agregar: ";
+                cin >> valor;
+                validarValor(valor);
+                list.append(valor);
+                break;
+            }
+            case 3: {
+                cout << "\nIngresa el valor a agregar: ";
+                cin >> valor;
+                validarValor(valor);
+                cout << "\nIngresa la posicion a insertar: ";
+                cin >> posicion;
+                list.insert(valor, posicion);
+                break;
+            }
+            case 4: {
+                cout << "\nIngresa la posicion a eliminar (ingresa '-1' para cancelar): ";
+                cin >> posicion;
+                if(posicion == -1) {
+                    break;
+                }
+                validarValor(posicion);
+                list.remove(posicion);
+                break;
+            }
+            case 5: {
+                if(list.listaVacia()){
+                    break;
+                }
+                cout << "\nLa suma de todos los valores de la lista es: " << int(list) << endl;
+                break;
+            }
+            case 6: {
+                if(list.listaVacia()){
+                    break;
+                }
+                ++list;
+                break;
+            }
+            case 7: {
+                if(list.listaVacia()){
+                    break;
+                }
+                cout << "\nIngresa el valor a restar: ";
+                cin >> valor;
+                validarValor(valor);
+                list - valor;
+                break;
+            }
+            case 8: {
+                list.print();
+                break;
+            }
+            case EXIT_CODE: {
+                cout << "\nRegresando al menu principal\n\n";
+                break;
+            }
+        }
+    }while (opcion != EXIT_CODE);
+}
+
 
 int main() {
     int opcion;
-    cout << "De que tipo va a ser la lista?\n1) Numeros enteros\n2) Numeros con decimales\n3) Caracteres\n\nOpcion: ";
-    cin >> opcion;
-    //checar que opcion ea un numero entero
-    while (opcion < 1 || opcion > 3) {
-        cin.clear();
-        cin.ignore();
-        cout << "\nOpcion no valida, intentalo de nuevo: ";
+    do{
+        cout << "De que tipo va a ser la lista?\n" <<
+            "1) Numeros enteros\n" <<
+            "2) Numeros con decimales\n" <<
+            "3) Caracteres\n" <<
+            "4) Cadenas de texto\n"
+            "0) Salir\n\n" <<
+            "Opcion: ";
         cin >> opcion;
-    }
-    switch (opcion) {
-        case 1: {
-            List<int> list;
-            funcionLista(opcion, list);
-            break;
+        validarOpcion(opcion, 3);
+
+        switch (opcion) {
+            case 1: {
+                List<int> list;
+                menuNumeros<int>(list);
+                break;
+            }
+            case 2: {
+                List<float> list;
+                menuNumeros<float>(list);
+                break;
+            }
+            case 3: {
+                List<char> list;
+                menuCaracteres<char>(list);
+                break;
+            }
+            case 4: {
+                List<string> list;
+                menuCaracteres<string>(list);
+                break;
+            }
+            case EXIT_CODE: {
+                cout << "\nSaliendo del programa\n\n";
+                break;
+            }
         }
-        case 2: {
-            List<float> list;
-            funcionLista(opcion, list);
-            break;
-        }
-        case 3: {
-            List<char> list;
-            funcionLista(opcion, list);
-            break;
-        }
-    }
+    }while(opcion != EXIT_CODE);
+    
     return 0;
 }
---
+++ Estructuras de datos/README.md
@@ -0,0 +1,7 @@
+# Estructuras-de-datos
+Programas y demás de la materia estructuras de datos y algoritmia
+
+## Libros de la materia
+- Sam's teach yourself 30 day C++
+- Programming principles and practice Bjarne
+- Implementing useful algorithms in C++
